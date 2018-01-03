daea00b807dd7b5a783cb7bb6f4bb73e5bee6926
tihonove <tihonove@skbkontur.ru>
2018-01-03 17:26:07 +0500

    Fix run script

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 // @flow
 import path from "path";
+import options from "commander";
 import BundleSetBuilder from "./BundleSetBuilder";
 import { BundleSetConfiguration } from "./BundleSetConfiguration";
 import BundleSets from "./BundleSets";
 
+options.option("-o, --output <path>", "Output directory").parse(process.argv);
+
 const sandboxDirectory = path.join(process.cwd(), "sandbox");
-const outputDirectory = path.join(process.cwd(), "..", "kontur-cdn.github.io");
+const outputDirectory = path.join(process.cwd(), options.output);
 
 for (const bundleSetName of Object.keys(BundleSets)) {
     const bundleSet = BundleSets[bundleSetName];
