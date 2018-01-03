4292e2786a9c5a64eb50a676b4c409c8e3d2b8ba
tihonove <tihonove@skbkontur.ru>
2018-01-03 12:15:19 +0500

    Исправил eslint ошибки

--- packages/kontur-cdn-webpack-plugin/rollup.config.js
+++ packages/kontur-cdn-webpack-plugin/rollup.config.js
@@ -1,33 +1,33 @@
-import babel from 'rollup-plugin-babel';
-import resolve from 'rollup-plugin-node-resolve';
-const pkg = require('./package.json');
+import babel from "rollup-plugin-babel";
+import resolve from "rollup-plugin-node-resolve";
+const pkg = require("./package.json");
 const external = Object.keys(pkg.dependencies || {}).concat(Object.keys(pkg.peerDependencies || {}));
 
 export default {
-    input: '../../src/WebpackPlugin/index.js',
+    input: "../../src/WebpackPlugin/index.js",
     output: {
-        file: 'index.js',
-        format: 'cjs',
+        file: "index.js",
+        format: "cjs",
     },
     plugins: [
-        resolve(),        
+        resolve(),
         babel({
             babelrc: false,
             presets: [
                 [
-                    'env',
+                    "env",
                     {
                         targets: {
-                            node: '4.0',
+                            node: "4.0",
                         },
                         modules: false,
-                        exclude: ['transform-regenerator'],
+                        exclude: ["transform-regenerator"],
                     },
                 ],
-                'flow',
+                "flow",
             ],
-            exclude: 'node_modules/**', // only transpile our source code
+            exclude: "node_modules/**", // only transpile our source code
         }),
     ],
-    external: external
+    external: external,
 };
